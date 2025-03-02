a7090d22558fb658d7cfb25a04d698e6c79fd8b9
GabrielC67 <gabecruzpro@gmail.com>
2025-03-02 18:05:14 -0500

    Updated as of 6:05pm

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,41 +1,15 @@
 package com.zipcodewilmington.scientificcalculator;
 import java.util.Scanner;
 
-import static com.zipcodewilmington.scientificcalculator.Scientific.scan;
-
 public class Calculator {
     public static Scanner CalcScan = new Scanner(System.in);
 
-        //This is #4
-        //1. A state, representing the value currently displayed on the calculator (default 0)
+    public double addTwoNum(double x, double y) {
+        return x + y;
+    }
 
-        //2. Get the current number on the display *
-
-        //3. Clear the display *
-
-        //4. Change the number on the display *
-
-        //5. Add, subtract, multiply, and divide the value on the display by a given number
-
-        //6. Calculate the square (x2) and square root (√x) of the number on the display *
-
-        //7. Calculate variable exponentiation (xy)
-
-        //8. Calculate the inverse of the number on the display (1/x) *
-
-        //9. Invert the sign of the number on the display (switch between positive and negative)
-
-        //10. Update the display to Err if an error occurs (eg: Division by zero) *
-
-        //11. Errors must be cleared before any other operation can take place *
-
-
-
-
-
-
-        }
-
-
+    public double subtractTwoNum(double x, double y){
+        return x - y;
+    }
 }
 
--- src/test/java/com/zipcodewilmington/scientific_calculator/scientific_calculator/TestCalculator.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/scientific_calculator/TestCalculator.java
@@ -6,5 +6,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCalculator {
 
-}
+        @Test
+        public void testAddTwoNum1() {
+            Calculator calculateAddition1 = new Calculator();
+            double actual = calculateAddition1.addTwoNum(3, 5);
+            assertEquals(8, actual, 0.0001);
+        }
+        @Test
+        public void testAddTwoNum2() {
+            Calculator calculateAddition2 = new Calculator();
+            double actual = calculateAddition2.addTwoNum(65, 130);
+            assertEquals(195, actual, 0.0001);
+        }
+
+        @Test
+        public void testAddTwoNum3() {
+        Calculator calculateAddition3 = new Calculator();
+        double actual = calculateAddition3.addTwoNum(14.3565, 359.432819);
+        assertEquals(373.789319, actual, 0.0001);
+        }
+
+        @Test
+        public void testSubtractTwoNum1() {
+        Calculator calculateSubtraction1 = new Calculator();
+        double actual = calculateSubtraction1.subtractTwoNum(9, 5);
+        assertEquals(4, actual, 0.0001);
+        }
+
+        @Test
+        public void testSubtractTwoNum2() {
+        Calculator calculateSubtraction2 = new Calculator();
+        double actual = calculateSubtraction2.subtractTwoNum(45.305, 16.89543);
+        assertEquals(28.40957, actual, 0.0001);
+        }
+
+        @Test
+        public void testSubtractTwoNum3() {
+        Calculator calculateSubtraction3 = new Calculator();
+        double actual = calculateSubtraction3.subtractTwoNum(794.47839210, 432.47832910);
+        assertEquals(362.000063, actual, 0.0001);
+    }
+    }
+
+
 
